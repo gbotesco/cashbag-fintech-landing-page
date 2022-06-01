6b20b9e9a788427466ea06058727bf7ac8e628dd
gbotesco <codex114@gmail.com>
2022-06-01 15:55:06 +0100

    build onboarding section

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            'primary-1': '#243BBF',
+            'primary-2': '#4F61CC',
+            'primary-dark': '#080D26',
+            'neutral': '#C9CED4',
+            'orange': '#ff7849',
+            'green': '#13ce66',
+            'yellow': '#ffc82c',
+            'white': '#FFFF',
+            'gray': '#8492a6',
+            'gray-light': '#d3dce6',
+        },
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
